90b2e638689783d1b68533089e1b00d8808c4828
Takehito Tada <taketada.works@gmail.com>
2021-03-07 00:50:38 +0900

    fix: tmp抹消

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :members 
+  resources :members
+
+  namespace :api do
+    resources :auth
+  end
 
   post '/signup', to: 'registrations#signup'
-
   post '/login', to: 'sessions#login'
   delete '/logout', to: 'sessions#logout'
   get '/logged_in', to: 'sessions#logged_in?'
