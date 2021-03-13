8954f95a33e792447358000cdf769e2fe4d20b5e
shiotani0011 <76035510+kizuneko1@users.noreply.github.com>
2021-03-13 17:06:30 +0900

    devise token auth

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :v1 do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'auth/registrations'}
+  end
+
   namespace :api do
     resources :auth do
       collection do
