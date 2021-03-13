9c9b88ead4d19d162ffb00f5553bf91144bc587e
shiotani0011 <76035510+kizuneko1@users.noreply.github.com>
2021-03-13 17:27:07 +0900

    Revert "devise token auth"
    This reverts commit 8954f95a33e792447358000cdf769e2fe4d20b5e.

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        
   skip_before_action :verify_authenticity_token
 
   helper_method :login!, :current_user
--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :validatable,
-                  :omniauthable
-            include DeviseTokenAuth::Concerns::User
-  # has_secure_password
+  has_secure_password
 
-    # validates :email, presence: true
-    # validates :email, uniqueness: true
-    # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
